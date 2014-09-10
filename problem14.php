d6f70e6ecaf87fb797bf4ccc116e9e6b8380e7c4
haydogsup <joshua.r.mays@gmail.com>
2014-09-10 17:12:02 -0500

    clarifying comments

--- problem14.php
+++ problem14.php
@@ -4,18 +4,22 @@ $chain = 1;
 $highChain = 1;
 $number = 1;
 
+// N(EVEN) => N/2 
 function even($num) {
     return $num / 2;
     
 }
 
+// N(ODD) => 3N + 1
 function odd($num) {
     return ($num * 3) + 1;
 }
 
-for($startNum = 1; $startNum < 1000000; $startNum += 2) {
+// LOOPING FROM 1 TO 999,999 TO FIND THE LONGEST COLLATZ CHAIN
+for($startNum = 1; $startNum < 1000000; $startNum++) {
     $num = $startNum;
 
+    // CHECKING IF NUMBER IS EVEN OR ODD AND APPLYING RULE
     while ($num > 1) {
         if ($num % 2 == 0) {
             $num = $num / 2;
@@ -23,12 +27,15 @@ for($startNum = 1; $startNum < 1000000; $startNum += 2) {
         else {
             $num = ($num * 3) + 1;
         }
+        // EACH TIME NUM DOES GET BACK TO ONE, INCREMENT THE CHAIN
         $chain++;
     }
+    // IF AN EQUAL OR LONGER CHAIN IS FOUND, MAKE IT THE NEW HIGHEST CHAIN
     if ($chain >= $highChain) {
         $highChain = $chain;
         $highNum = $startNum;
     }
+    // RESET CHAIN BACK TO ONE FOR NEXT STARTING NUMBER.
     $chain = 1;
 }
     echo "The longest sequence contains " . $highChain . " terms." . PHP_EOL;
